25fb6b13fab6891f99b450829f02a6b69ca9758d
Nicholas English <englisni@arch07.cis.gvsu.edu>
2018-04-22 23:34:46 -0400

    Initial setup of the program.

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+/**********************************************************************
+ * This code solves a Sudoku game by the use of the backtracking 
+ * technique. The code will then print the solved Sudoku game at the
+ * end of it's run.
+ *
+ * @author Nicholas English
+ * @version 1.0
+ * @date 4/22/2018
+ *********************************************************************/
+
+/* Librarie(s) */
+#include <iostream>
+#include <vector>
+#include <string>
+
+/* Main Function */
+int main(int argc, char *argv[]) {
+
+
+    /* End of program reached */
+    std::cout << "\n\n\n" << std::ebdl;
+    return 0;
+}
+
